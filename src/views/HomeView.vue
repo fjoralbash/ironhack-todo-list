54a2120993d54a31c0e0513c6f10ae47d2cb9049
Fjoralba Shehaj <fjoralba.shehaj@fti.edu.al>
2023-08-20 21:26:57 +0200

    new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   src/App.vue 	new file:   src/assets/main.css 	new file:   src/lib/supabase.js 	new file:   src/main.js 	new file:   src/router/index.js 	new file:   src/stores/user.js 	new file:   src/views/HomeView.vue 	new file:   vite.config.js

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+import { useUserStore }  from '@/stores/user';
+
+const userStore = useUserStore();
+const email = ref("");
+const password = ref("");
+
+</script>
+
+<template>
+<h1>ToDo List</h1>
+<h2 v-if="userStore.user">Current User: {{ userStore.user.user.email }}</h2>
+<input placeholder="Write your email" v-model="email">
+<input placeholder="Write your password" v-model="password">
+<button @click="userStore.createNewUser(email, password)">Create New User</button>
+</template>
